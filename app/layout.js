26e8472795e6f8ed33186600fce464783beeb2e9
agoracuidar-art <agoracuidar@gmail.com>
2026-09-10 10:12:06 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Mãe & Cuidado',
+  description: 'Aplicativo PWA Mãe & Cuidado',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
