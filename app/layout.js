d41e6201502a0e1612028ba653e13206a10df0f5
agoracuidar-art <agoracuidar@gmail.com>
2026-09-10 11:01:40 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,16 @@
 export const metadata = {
   title: 'Mãe & Cuidado',
-  description: 'Aplicativo PWA Mãe & Cuidado',
+  description: 'Aplicativo Mãe & Cuidado',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0, fontFamily: 'sans-serif' }}>
+        {children}
+      </body>
     </html>
   )
 }
+
+
